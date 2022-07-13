20b91e88de167a2e6816e9782a3edc7be3d711f4
silva <pysilva@qq.com>
2022-07-13 11:13:56 +0800

    重置

--- TLNewsSpider/TLNewsSpider/package/rules/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-    集合所有采集规则
-'''
-from .titles import TitleRules
-from .content import ContentRules
-from .publication import PublishDateRules
-from .attachment import AttachmentDownloadRules
-from .offices import OfficeRules
-from .author import AuthorExtractor
